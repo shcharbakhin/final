b13083603fb9f4df892e3ff8acdcd8f9e777e612
shcharbakhin <113371130+shcharbakhin@users.noreply.github.com>
2022-11-20 10:17:41 +0300

    Решение задачи

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            string[] final = { };
+            string[] arr = { "hello", "world", "coomit", "22b", "ad" };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string str = arr[i];
+                int x = str.Length;
+                if (x < 4)
+                {
+                    int j = 0;
+                    final[j] = str;
+                    Console.Write(final[j]);
+                    j++;
+                }
+            }
+        }
+    }
+}
